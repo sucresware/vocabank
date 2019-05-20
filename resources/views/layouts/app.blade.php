dd2016842f82731fede407f559a0f76530c5ff7f
Simon MGK <contact@mgkprod.fr>
2019-05-20 02:30:42 +0200

    Updated GA tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,13 +27,13 @@
 
     @if (config('app.env') === 'production')
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139755516-1"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139755516-2"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'UA-139755516-1');
+            gtag('config', 'UA-139755516-2');
         </script>
     @endif
 
