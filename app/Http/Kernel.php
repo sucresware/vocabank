3962e1839d51153a0e3be3cdbfec5e95de486474
Simon MGK <contact@mgkprod.fr>
2020-04-05 14:30:30 +0200

    Add cors alias to kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
         'role'               => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission'         => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'cors'               => \Spatie\Cors\Cors::class,
     ];
 
     /**
